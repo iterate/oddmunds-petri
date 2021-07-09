19c14e1b267ddc64014031eb66a6cc162f0b0adc
Oddmund <oddmunds@iterate.no>
2021-07-09 22:24:40 +0200

    set url

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :petrix, PetrixWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "oddmunds-petri.test.iterate.no", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
